f7d843129fc5b903702c0aa31fc647c5be456364
al <albertincx@gmail.com>
2018-06-20 23:40:22 +0300

    add redux-persist

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -1,10 +1,22 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from '../reducers'
+import weatherMiddleware from './weatherMiddleware'
 
-export const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware,
-  )
-)
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export default () => {
+  let store = createStore(persistedReducer,
+    applyMiddleware(
+      thunkMiddleware,
+      weatherMiddleware
+    ))
+  let persistor = persistStore(store)
+  return {store, persistor}
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { store } from './helpers'
+import persisStore from './helpers/store'
+import { PersistGate } from 'redux-persist/lib/integration/react'
 import CityContainer from './components/CityContainer'
 import './css/style.css'
-
+let store = persisStore()
 render(
-  <Provider store={store}>
-    <CityContainer />
+  <Provider store={store.store}>
+    <PersistGate loading={null} persistor={store.persistor}>
+      <CityContainer />
+    </PersistGate>
   </Provider>,
   document.getElementById('app')
 )
